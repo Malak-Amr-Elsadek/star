a7d5933fd1db3a7650a5500766e35e5db0a864da
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-12-15 15:47:18 +0300

    star

---
+++ main.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.Screen().bgcolor("Hot pink")
+turtle.Screen().setup(300,400)
+
+pen =turtle.Turtle()
+pen.color("violet")
+pen.width(10)
+
+
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+pen.penup()
+pen.right(150)
+pen.forward(50)
+
+pen.pendown()
+pen.right(90)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+pen.right(120)
+pen.forward(100)
+
+turtle.done()
